f4ad6acdd2b2b312a13de08817436a658af00c7a
Paul Rigge <rigge@berkeley.edu>
2018-12-11 17:46:51 +0000

    Use github URL for SBT

--- .install_sbt.sh
+++ .install_sbt.sh
@@ -2,7 +2,7 @@ set -e
 if [ ! -f $INSTALL_DIR/bin/sbt ]; then
   mkdir -p $INSTALL_DIR
   pushd $INSTALL_DIR
-  curl -sS https://piccolo.link/sbt-1.2.7.zip
+  curl -sS https://github.com/sbt/sbt/releases/download/v1.2.7/sbt-1.2.7.zip
   unzip sbt-1.2.7.zip
   rm sbt-1.2.7.zip
   popd
