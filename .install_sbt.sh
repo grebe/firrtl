e4635b9e50e52c46a33e7f84498dca53a5384f52
Paul Rigge <rigge@berkeley.edu>
2018-12-11 17:52:44 +0000

    Don't use silent mode for curl

--- .install_sbt.sh
+++ .install_sbt.sh
@@ -2,7 +2,8 @@ set -ex
 if [ ! -f $INSTALL_DIR/bin/sbt ]; then
   mkdir -p $INSTALL_DIR
   pushd $INSTALL_DIR
-  curl -sS https://github.com/sbt/sbt/releases/download/v1.2.7/sbt-1.2.7.zip
+  curl https://github.com/sbt/sbt/releases/download/v1.2.7/sbt-1.2.7.zip
+  ls
   unzip sbt-1.2.7.zip
   rm sbt-1.2.7.zip
   popd
